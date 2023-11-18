75d7b620ee60bedf599a2e052acca0a1dc35961a
byteYuFan <854978151@qq.com>
2023-11-18 21:32:57 +0800

    ok

---
+++ cmake-build-debug/CMakeFiles/yftcp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yftcp.dir/main.cpp.o"
+  "CMakeFiles/yftcp.dir/main.cpp.o.d"
+  "yftcp"
+  "yftcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yftcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/yftcp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for yftcp.
+# This may be replaced when dependencies are built.
